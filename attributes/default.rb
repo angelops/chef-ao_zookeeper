b7bd7543a44dbf957898944147e939e7a80fed4d
Justin Alan Ryan <juryan@ziprealty.com>
2016-01-25 12:02:12 -0800

    set zk version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 node.default['java']['jdk_version'] = '8'
 node.default["zookeeper"]["myid"] = '-1'
 node.default["zookeeper"]["service_style"] = 'runit'
+node.default["zookeeper"]["version"] = '3.4.6'
 node.default["zookeeper"]["checksum"] = '971c379ba65714fd25dc5fe8f14e9ad1'
