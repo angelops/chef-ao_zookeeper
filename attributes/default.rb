f68082560a65d80436a53d1835016cdceb5eeb0e
Justin Alan Ryan <juryan@ziprealty.com>
2016-01-25 11:54:38 -0800

    updated checksum.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 node.default['java']['jdk_version'] = '8'
 node.default["zookeeper"]["myid"] = '-1'
 node.default["zookeeper"]["service_style"] = 'runit'
-
+node.default["zookeeper"]["checksum"] = '971c379ba65714fd25dc5fe8f14e9ad1'
