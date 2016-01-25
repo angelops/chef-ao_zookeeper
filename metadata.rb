42e06af8a81b901628b6c4bd703ef0a880b3364e
Justin Alan Ryan <juryan@ziprealty.com>
2016-01-25 12:09:56 -0800

    sha256 checksum, not md5.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'juryan@ziprealty.com'
 license          'Apache 2.0'
 description      'Installs/Configures zip_zookeeper'
 long_description 'Installs/Configures zip_zookeeper'
-version          '0.1.6'
+version          '0.1.7'
 
 depends 'java'
 depends 'zookeeper'
