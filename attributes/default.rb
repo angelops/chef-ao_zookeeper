17f13eda94f9710880553c21bd4dac3d4e5fdc33
Justin Alan Ryan <juryan@ziprealty.com>
2016-07-08 12:14:59 -0700

    fix name error

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ node.default["zookeeper"]["version"] = '3.4.6'
 node.default["zookeeper"]["checksum"] = '01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994'
 node.default["zookeeper"]["user_home"] = '/srv/zookeeper'
 node.default["zookeeper"]["mirror"] = 'http://apache.mirrors.tds.net/zookeeper/'
-default[:zip_zookeeper][:zk_cleanup_keep_count] = 3
-default[:zip_zookeeper][:cron_cleanup_hour] = 12
-default[:zip_zookeeper][:cron_cleanup_minute] = 10
+default[:zip_zookeeper][:cleanup_keep_count] = 3
+default[:zip_zookeeper][:cron_cleanup_hour] = 0
+default[:zip_zookeeper][:cron_cleanup_minute] = 0
 
